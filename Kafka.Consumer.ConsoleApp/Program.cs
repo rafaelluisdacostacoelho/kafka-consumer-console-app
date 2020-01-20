85855b3e0ff482c43d89a6199aa62f2ee6472ebd
Rafael Luis da Costa Coelho <rafael.coelho@autoglass.com.br>
2020-01-20 16:51:41 -0300

    Add the kafka consumer prototype.

---
+++ Kafka.Consumer.ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace Kafka.Consumer.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "chat-consumer-group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe("chat-topic");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = consumer.Consume(cts.Token);
+                            Console.WriteLine($"Consumed message: '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException exception)
+                        {
+                            Console.WriteLine($"Error ocurred: {exception.Error.Reason}");
+                        }
+
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    consumer.Close();
+                }
+            }
+        }
+    }
+}
